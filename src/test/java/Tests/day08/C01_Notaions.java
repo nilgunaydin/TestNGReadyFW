5bb9491fae5d3a0ce613d1b7ee782ef16b499e25
Nilgun <94299742+nilgunaydin@users.noreply.github.com>
2022-01-25 15:13:58 +0300

    first commit

---
+++ src/test/java/Tests/day08/C01_Notaions.java
@@ -0,0 +1,21 @@
+package Tests.day08;
+
+import org.testng.annotations.Test;
+
+public class C01_Notaions {
+
+    @Test
+    public void youTube(){
+        System.out.println("youtube testi calisti");
+    }
+
+  @Test
+    public void amazon(){
+      System.out.println("amazon testi calisti");
+  }
+
+    @Test
+    public void bestbuy(){
+        System.out.println("bestbuy testi calisti");
+    }
+}
