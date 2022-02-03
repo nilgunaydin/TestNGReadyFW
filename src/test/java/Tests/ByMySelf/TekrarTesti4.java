e57426e63507ef0b662af18a007121143f58bca0
Nilgun <94299742+nilgunaydin@users.noreply.github.com>
2022-02-03 17:09:51 +0300

    some hw

--- src/test/java/Tests/ByMySelf/TekrarTesti4.java
+++ src/test/java/Tests/ByMySelf/TekrarTesti4.java
@@ -1,11 +1,49 @@
 package Tests.ByMySelf;
 
-public class TekrarTesti4 {
-    //1.“http://webdriveruniversity.com/IFrame/index.html” sayfasina gidin
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.locators.RelativeLocator;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utilities.TestBase;
+
+public class TekrarTesti4 extends TestBase {
+
+    @Test
+    public void test(){
+    //1. sayfasina gidin
+    driver.get("http://webdriveruniversity.com/IFrame/index.html");
     //2.“Our Products” butonuna basin
+        WebElement iFrame=driver.findElement(By.tagName("iframe"));
+
+        driver.switchTo().frame(iFrame);
+
+        driver.findElement(By.xpath("//html/body/div[1]/div/div//div/ul/li[2]/a")).click();
+
+
+
     //3.“Cameras product”i tiklayin
+        driver.findElement(By.xpath("//img[@id='camera-img']")).click();
+
+        driver.switchTo().parentFrame();
     //Popup mesajini yazdirin
-    //5.“close” butonuna basin
+        driver.switchTo().frame(iFrame);
+
+        String text = driver.findElement(By.xpath("//div[@class='modal-body']")).getText();
+        System.out.println(text);
+
+
+        //5.“close” butonuna basin
+
+        driver.findElement(By.xpath("//button[@class='btn btn-default']")).click();
+
+        driver.switchTo().parentFrame();
     //6."WebdriverUniversity.com (IFrame)" linkini tiklayin
-    //7."http://webdriveruniversity.com/index.html" adresine gittigini test edin
-}
+        driver.findElement(By.xpath("//a[text()='WebdriverUniversity.com (IFrame)']")).click();
+
+    //7."" adresine gittigini test edin
+Assert.assertTrue(driver.getCurrentUrl().equals("http://webdriveruniversity.com/index.html"));
+
+
+    }
+ }
--- src/test/java/Tests/odev/odev06.java
+++ src/test/java/Tests/odev/odev06.java
@@ -1,12 +1,42 @@
 package Tests.odev;
 
-public class odev06 {
-    //● https://the-internet.herokuapp.com/windows adresine gidin.
-    //● Sayfadaki textin “Opening a new window” olduğunu doğrulayın.
-    //● Sayfa başlığının(title) “The Internet” olduğunu doğrulayın.
-    //● Click Here butonuna basın.
-    //● Acilan yeni pencerenin sayfa başlığının (title) “New Window” oldugunu dogrulayin.
-    //● Sayfadaki textin “New Window” olduğunu doğrulayın.
-    //● Bir önceki pencereye geri döndükten sonra sayfa başlığının “The Internet” olduğunu doğrulayın.
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utilities.TestBase;
 
+import java.util.Set;
+
+public class odev06 extends TestBase {
+    @Test
+    public void test(){
+
+    // https://the-internet.herokuapp.com/windows adresine gidin.
+        driver.get("https://the-internet.herokuapp.com/windows");
+    // Sayfadaki textin “Opening a new window” olduğunu doğrulayın.
+        String text = driver.findElement(By.tagName("h3")).getText();
+        Assert.assertTrue(text.equals("Opening a new window"));
+    // Sayfa başlığının(title) “The Internet” olduğunu doğrulayın.
+        Assert.assertTrue(driver.getTitle().equals("The Internet"));
+    // Click Here butonuna basın.
+        String Handle1 = driver.getWindowHandle();
+        driver.findElement(By.xpath("//a[text()='Click Here']")).click();
+    Set<String> handles = driver.getWindowHandles();
+    String Handle2="";
+        for (String handle : handles) {
+if (handle!=Handle1) Handle2=handle;
+        }
+        driver.switchTo().window(Handle2);
+        // Acilan yeni pencerenin sayfa başlığının (title) “New Window” oldugunu dogrulayin.
+
+        Assert.assertTrue(driver.getTitle().equals("New Window"));
+    // Sayfadaki textin “New Window” olduğunu doğrulayın.
+        text = driver.findElement(By.tagName("h3")).getText();
+        Assert.assertTrue(text.equals("New Window"));
+    // Bir önceki pencereye geri döndükten sonra sayfa başlığının “The Internet” olduğunu doğrulayın.
+driver.switchTo().window(Handle1);
+Assert.assertTrue(driver.getTitle().equals("The Internet"));
+
+
+    }
 }
